16dc20e761f2f1ed20b6fbd7b76092c579e43141
Dev <devkanojia1962000@gmail.com>
2023-12-30 15:33:25 +0530

    second commit

---
+++ dgyub.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxSubArray(vector<int>& nums) {
+        long long currsum=nums[0],sum=0;
+        for(int i=0;i<nums.size()-1;i++){
+            sum=nums[i];
+            for(int j=i+1;i<nums.size();j++){
+                sum+=nums[j];
+                if(sum>=currsum)
+                    currsum=sum;
+            }
+        }
+        return currsum;
+    }
+int main(){
+    vector<int>vc={-2,1,-3,4,-1,2,1,-5,4};
+    cout<<maxSubArray(vc);
+}
---
+++ gdvf.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        string st;
+        cin>>st;
+        if(st=='abc'|| st=='acb'||st=='bac'||)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        cout<<"\n";
+    }
+}
